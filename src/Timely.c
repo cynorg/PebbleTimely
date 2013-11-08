37c24c3f7af6dff68b12e0cc2b22edb05b23aa9c
Ben Johnson <circuitben@gmail.com>
2013-11-08 10:41:18 -0800

    Initial port to SDK 2.0.

--- src/Timely.c
+++ src/Timely.c
@@ -1,8 +1,5 @@
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include <pebble.h>
 #include <math.h>
-#include "resource_ids.auto.h"
 #include "config.h"
 
 /*
@@ -39,6 +36,7 @@
 #define UUID_DARK { 0x55, 0xF7, 0x74, 0x4B, 0xF5, 0x86, 0x45, 0xB8, 0x85, 0x4C, 0x55, 0x4F, 0x34, 0x48, 0x9F, 0x21 }
 #define UUID_LIGHT { 0x55, 0xF7, 0x74, 0x4B, 0xF5, 0x86, 0x45, 0xB8, 0x85, 0x4C, 0x55, 0x4F, 0x34, 0x48, 0x9F, 0x22 }
 
+#if 0
 #ifdef TIMELY_DARK
 PBL_APP_INFO(UUID_DARK,
              "Timely Dark", "Martin Norland (@cynorg)",
@@ -53,13 +51,14 @@ PBL_APP_INFO(UUID_LIGHT,
              APP_INFO_WATCH_FACE);
 
 #endif
+#endif
 
-Window window;
+Window *window;
 
-TextLayer text_time_layer;
+TextLayer *text_time_layer;
 
-Layer month_layer;
-Layer days_layer;
+Layer *month_layer;
+Layer *days_layer;
 
 #ifdef TIMELY_DARK
 const bool black = true;       // Is the background black: don't change this, refer to the CONFIGURATION section at the top
@@ -119,12 +118,10 @@ int daysInMonth(int mon, int year){
 
 void setColors(GContext* ctx){
     if(black){
-        window_set_background_color(&window, GColorBlack);
         graphics_context_set_stroke_color(ctx, GColorWhite);
         graphics_context_set_fill_color(ctx, GColorBlack);
         graphics_context_set_text_color(ctx, GColorWhite);
     }else{
-        window_set_background_color(&window, GColorWhite);
         graphics_context_set_stroke_color(ctx, GColorBlack);
         graphics_context_set_fill_color(ctx, GColorWhite);
         graphics_context_set_text_color(ctx, GColorBlack);
@@ -137,7 +134,6 @@ void setInvColors(GContext* ctx){
         graphics_context_set_fill_color(ctx, GColorBlack);
         graphics_context_set_text_color(ctx, GColorWhite);
     }else{
-        window_set_background_color(&window, GColorWhite);
         graphics_context_set_stroke_color(ctx, GColorBlack);
         graphics_context_set_fill_color(ctx, GColorWhite);
         graphics_context_set_text_color(ctx, GColorBlack);
@@ -150,8 +146,9 @@ void days_layer_update_callback(Layer *me, GContext* ctx) {
     int j;
     int i;
     
-    PblTm currentTime;
-    get_time(&currentTime);
+    time_t tt = time(0);
+    struct tm currentTime = *localtime(&tt);
+
     int mon = currentTime.tm_mon;
     int year = currentTime.tm_year+1900;
     int daysThisMonth = daysInMonth(mon,year);
@@ -271,7 +268,7 @@ void days_layer_update_callback(Layer *me, GContext* ctx) {
         j = i+dayOfWeekOffset;
         if(j>6) j-=7;
         if(j<0) j+=7;
-        graphics_text_draw(
+        graphics_draw_text(
             ctx, 
             daysOfWeek[j], 
             fonts_get_system_font(dayFonts[whichDayFont]), 
@@ -321,7 +318,7 @@ void days_layer_update_callback(Layer *me, GContext* ctx) {
         }
 
         // Draw the day
-        graphics_text_draw(
+        graphics_draw_text(
             ctx, 
             intToStr(calendar[i]),  
             font, 
@@ -343,19 +340,19 @@ void days_layer_update_callback(Layer *me, GContext* ctx) {
 
 void month_layer_update_callback(Layer *me, GContext* ctx) {
     (void)me;
-    PblTm currentTime;
-    get_time(&currentTime);
-    
+
+    time_t tt = time(0);
+    struct tm *currentTime = localtime(&tt);
+
     setColors(ctx);
-    
+
     char str[20] = ""; 
     // http://www.cplusplus.com/reference/ctime/strftime/
-    string_format_time(str, sizeof(str), "%B %d, %Y", &currentTime); // Month DD, YYYY
-    //string_format_time(str, sizeof(str), "%d.%m.%Y", &currentTime);  // DD.MM.YYYY
+    strftime(str, sizeof(str), "%B %d, %Y", currentTime); // Month DD, YYYY
+    //strftime(str, sizeof(str), "%d.%m.%Y", currentTime);  // DD.MM.YYYY
 
-    
     // Draw the MONTH/YEAR String
-    graphics_text_draw(
+    graphics_draw_text(
         ctx, 
         str,  
         fonts_get_system_font(FONT_KEY_GOTHIC_24), 
@@ -365,7 +362,7 @@ void month_layer_update_callback(Layer *me, GContext* ctx) {
         NULL);
 }
 
-void paint_time(PblTm* currentTime) {
+void paint_time(struct tm* currentTime) {
   // Need to be static because used by the system later.
   static char time_text[] = "00:00";
 
@@ -377,7 +374,7 @@ void paint_time(PblTm* currentTime) {
     time_format = "%I:%M";
   }
 
-  string_format_time(time_text, sizeof(time_text), time_format, currentTime);
+  strftime(time_text, sizeof(time_text), time_format, currentTime);
 
   // Kludge to handle lack of non-padded hour format string
   // for twelve hour clock.
@@ -385,52 +382,47 @@ void paint_time(PblTm* currentTime) {
     memmove(time_text, &time_text[1], sizeof(time_text) - 1);
   }
 
-  text_layer_set_text(&text_time_layer, time_text);
-
+  text_layer_set_text(text_time_layer, time_text);
 }
 
-void handle_init(AppContextRef ctx) {
-  (void)ctx;
-    window_init(&window, "Calendar");
-    window_stack_push(&window, false /* Animated */);
-    window_set_fullscreen(&window, true);
-    
-    setColors(ctx);
-    
-    layer_init(&month_layer, window.layer.frame);
-    month_layer.update_proc = &month_layer_update_callback;
-    layer_add_child(&window.layer, &month_layer);
+void handle_init() {
+    window = window_create();
+    window_stack_push(window, false /* Animated */);
+    window_set_fullscreen(window, true);
 
-    layer_init(&days_layer, window.layer.frame);
-    days_layer.update_proc = &days_layer_update_callback;
-    layer_add_child(&window.layer, &days_layer);
-    
-    resource_init_current_app(&APP_RESOURCES);
+    if (black) {
+        window_set_background_color(window, GColorBlack);
+    } else {
+        window_set_background_color(window, GColorWhite);
+    }
 
-  text_layer_init(&text_time_layer, GRect(0, 26, 144, 168-22));
+    month_layer = layer_create(layer_get_frame(window_get_root_layer(window)));
+    layer_set_update_proc(month_layer, month_layer_update_callback);
+    layer_add_child(window_get_root_layer(window), month_layer);
+
+    days_layer = layer_create(layer_get_frame(window_get_root_layer(window)));
+    layer_set_update_proc(days_layer, days_layer_update_callback);
+    layer_add_child(window_get_root_layer(window), days_layer);
+    
+  text_time_layer = text_layer_create(GRect(0, 26, 144, 168-22));
   if(black){
-    text_layer_set_text_color(&text_time_layer, GColorWhite);
-    text_layer_set_background_color(&text_time_layer, GColorClear);
+    text_layer_set_text_color(text_time_layer, GColorWhite);
+    text_layer_set_background_color(text_time_layer, GColorClear);
   }else{
-    text_layer_set_text_color(&text_time_layer, GColorBlack);
-    text_layer_set_background_color(&text_time_layer, GColorClear);
+    text_layer_set_text_color(text_time_layer, GColorBlack);
+    text_layer_set_background_color(text_time_layer, GColorClear);
   }
-  text_layer_set_font(&text_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_49)));
-  text_layer_set_text_alignment(&text_time_layer, GTextAlignmentCenter);
-  layer_add_child(&window.layer, &text_time_layer.layer);
+  text_layer_set_font(text_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_49)));
+  text_layer_set_text_alignment(text_time_layer, GTextAlignmentCenter);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_time_layer));
 
-  PblTm currentTime;
-  get_time(&currentTime);
-  paint_time(&currentTime);
-
+  time_t tt = time(0);
+  paint_time(localtime(&tt));
 }
 
-void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
-
-  (void)ctx;
-
-  paint_time(t->tick_time);
-  if (t->tick_time->tm_min == 0 && t->tick_time->tm_sec == 0) {
+void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
+  paint_time(tick_time);
+  if (tick_time->tm_min == 0 && tick_time->tm_sec == 0) {
     // top of the hour, not simply changing to the watchface at 0 minutes past (tick handler is called after init)
     if (vibe_hour) { vibes_short_pulse(); }
 
@@ -440,19 +432,13 @@ void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
     // TODO:  http://forums.getpebble.com/discussion/4597/timers-and-click-handlers-dont-mix
     // TODO:  and http://forums.getpebble.com/discussion/4946/layers
     // TODO: Note: it's unclear, right now, if redrawing *any* layer redraws the whole screen ;(
-    layer_mark_dirty(&month_layer);
-    layer_mark_dirty(&days_layer);
+    layer_mark_dirty(month_layer);
+    layer_mark_dirty(days_layer);
   }
 }
 
-void pbl_main(void *params) {
-  PebbleAppHandlers handlers = {
-    .init_handler = &handle_init,
-
-    .tick_info = {
-        .tick_handler = &handle_minute_tick,
-        .tick_units = MINUTE_UNIT
-    }
-  };
-  app_event_loop(params, &handlers);
+int main() {
+  handle_init();
+  tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+  app_event_loop();
 }
